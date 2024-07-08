fcfb6da922269100784e125fdfcc97067809bd77
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-07-08 03:30:31 +0300

    add migrations

---
+++ database/migrations/2024_07_07_071738_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->double('weight');
+            $table->integer('region');
+            $table->json('delivery_hours');
+            $table->string('assign_time')->nullable();
+            $table->timestamps();
+
+            $table->foreignId('courier_id')->nullable()
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
