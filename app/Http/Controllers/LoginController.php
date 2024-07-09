130401931d2dc9c4ae5c56c536e839b662a858fd
AJIEXAHDPO <alesha.iv03@gmail.com>
2024-07-09 06:39:21 +0300

    fix token logout error

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,12 +18,12 @@ class LoginController extends Controller
 
         if (Auth::attempt($credentails)) {
             $user = $request->user();
-            $token = $user->createToken($user->email);
+            $token = $user->createToken($user->email, ['server:update']);
 
             return response()->json([
                 'token' => $token->plainTextToken,
                 'tokken_type' => 'Bearer',
-                'user' => $user
+                'user' => new UserResource($user)
             ], 200);
         }
         return response()->json(['message' => 'Invalid login details'], 401)->header('Content-Type', 'application/json');
@@ -30,7 +31,7 @@ class LoginController extends Controller
 
     public function logout(Request $request)
     {
-        $request->user()->currentAccessToken()->delete();
+        $request->user()->tokens()->delete();
         return response()->json(['message' => 'Logged out'], 200);
     }
 }
